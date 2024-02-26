1748ec9979e4ce0b434725ed092c9f858f989124
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-02-26 14:49:13 +0900

    EntityManger로 CRUD 작성하기 (CRU까지 진행)

---
+++ chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section02/crud/A_EntityManagerCRUDTests.java
@@ -0,0 +1,100 @@
+package com.ohgiraffers.section02.crud;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class A_EntityManagerCRUDTests {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @Test
+    void 메뉴코드로_메뉴_조회_테스트() {
+
+        // given
+        int menuCode = 2;
+
+        // when
+        Menu foundMenu = entityManager.find(Menu.class, menuCode);
+
+        // then
+        Assertions.assertNotNull(foundMenu);
+        Assertions.assertEquals(menuCode, foundMenu.getMenuCode());
+        System.out.println("foundMenu = " + foundMenu);
+    }
+
+    @Test
+    void 새로운_메뉴_추가_테스트() {
+
+        // given
+        Menu menu = new Menu();
+        menu.setMenuName("김치제육볶음");
+        menu.setMenuPrice(6000);
+        menu.setCategoryCode(4);
+        menu.setOrderableStatus("Y");
+
+        // when
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        try {
+            entityManager.persist(menu);
+            entityTransaction.commit();
+        } catch (Exception e) {
+            entityTransaction.rollback();
+        }
+
+        // then
+        Assertions.assertTrue(entityManager.contains(menu));        // menu 객체가 현재 영속 상태로 관리되는지 확인
+    }
+
+    @Test
+    void 메뉴_이름_수정_테스트() {
+
+        // given
+        Menu menu = entityManager.find(Menu.class, 2);
+        System.out.println("menu = " + menu);
+
+        String menuNameToChange = "갈치스무디";
+
+        // when
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        try {
+            menu.setMenuName(menuNameToChange);
+            entityTransaction.commit();
+        } catch (Exception e) {
+            entityTransaction.rollback();
+        }
+
+        // then
+        Assertions.assertEquals(menuNameToChange, entityManager.find(Menu.class, 2).getMenuName());
+
+    }
+
+}
