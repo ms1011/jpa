d6de5b3d8c812b067a78a90def26b3cc20993d87
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-02-27 14:36:01 +0900

    일대다, 다대다, 양방향 연관관계

---
+++ chap03-assoication-lecture-source/src/test/java/com/ohgiraffers/section02/onetomany/CategoryAndMenu.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section02.onetomany;
+
+import com.ohgiraffers.section01.manytoone.Category;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "category_and_menu")
+@Table(name = "tbl_category")
+public class CategoryAndMenu {
+
+    @Id
+    @Column(name = "category_code")
+    private int categoryCode;
+    @Column(name = "category_name")
+    private String categoryName;
+    @Column(name = "ref_category_code")
+    private Integer refCategoryCode;
+
+    @JoinColumn(name = "category_code")
+    @OneToMany
+    private List<Menu> menuList;
+}
