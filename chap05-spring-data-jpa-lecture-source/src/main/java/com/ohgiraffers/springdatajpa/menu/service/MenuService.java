db8ae34aab8dc484deb89c96636445a03560516b
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-02-28 13:58:14 +0900

    페이징 처리

--- chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
@@ -6,6 +6,9 @@ import com.ohgiraffers.springdatajpa.menu.repository.CategoryRepository;
 import com.ohgiraffers.springdatajpa.menu.repository.MenuRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -46,5 +49,28 @@ public class MenuService {
     }
 
 
+    /* 설명. 3. findAll(페이징 처리 후) */
+    public Page<MenuDTO> findMenuList(Pageable pageable) {
 
+        /* 설명.
+         *  1. 넘어온 pageable에 담긴 페이지 번호를 인덱스 개념으로 바꿔서 인지 시킴
+         *  2. 한 페이지에 뿌려질 데이터 크기
+         *  3. 정렬 기준
+        * */
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1,
+                                    pageable.getPageSize(),
+                                    Sort.by("menuCode").descending());
+
+        Page<Menu> menuList = menuRepository.findAll(pageable);
+
+        return menuList.map(menu -> mapper.map(menu, MenuDTO.class));
+    }
+
+    public List<MenuDTO> findMenuPrice(int menuPrice) {
+
+        /* 설명. 전달 받은 가격을 초과하는 메뉴의 목록을 조회하는 메소드 */
+        List<Menu> menuList = menuRepository.findByMenuPriceGreaterThan(menuPrice);
+
+        return menuList.stream().map(menu -> mapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+    }
 }
