1a8489aa828dff27bbe724295b807afa18257cca
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-02-28 11:16:48 +0900

    메뉴 리스트 조회 기능 추가

--- chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/menu")
 @Slf4j
@@ -41,4 +43,13 @@ public class MenuController {
         return "menu/detail";
     }
 
+    /* 설명. 페이징 처리 전*/
+    @GetMapping("/list")
+    public String findMenuList(Model model) {
+
+        List<MenuDTO> menuList = menuService.findMenuList();
+        model.addAttribute("menuList", menuList);
+
+        return "menu/list";
+    }
 }
--- chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
@@ -6,7 +6,11 @@ import com.ohgiraffers.springdatajpa.menu.repository.CategoryRepository;
 import com.ohgiraffers.springdatajpa.menu.repository.MenuRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /* 설명.
  *  Service 계층 : 비즈니스 로직, 트랜잭션 처리(@Transactional), DTO <-> Entity(modelmapper lib 활용)
@@ -25,11 +29,22 @@ public class MenuService {
         this.categoryRepository = categoryRepository;
     }
 
+    /* 설명. 1. findById 예제 */
     public MenuDTO findMenuByCode(int menuCode) {
 
         Menu menu = menuRepository.findById(menuCode).orElseThrow(IllegalArgumentException::new);
 
         return mapper.map(menu, MenuDTO.class);
     }
+
+    /* 설명. 2. findAll(페이징 처리 전) */
+    public List<MenuDTO> findMenuList() {
+
+        List<Menu> menuList = menuRepository.findAll(Sort.by("menuCode").descending());
+
+        return menuList.stream().map(menu -> mapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+    }
+
+
 
 }
